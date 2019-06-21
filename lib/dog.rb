4bf49bb867618bb785e64bf23235ace2915f9e9a
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-06-21 16:57:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
-  def initialize(name)
-    @name = name
-  end
   def name
     @name
   end
+  def name=(name)
+    @name = name
+  end
 end
-fido=Dog.new("Fido")
-fido.name
+
+fido.name = "Fido"
